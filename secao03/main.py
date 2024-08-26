01d49edd5a773f611a33fff0271fa334f9a4e459
renatorv <renato@volus.coms>
2024-08-26 14:30:21 -0300

    Finalizada aula 18

--- secao03/main.py
+++ secao03/main.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 
 from fastapi.responses import JSONResponse
 from fastapi import Response
+from fastapi import Path
+from fastapi import Query
 
 from fastapi import FastAPI
 from fastapi import HTTPException
@@ -37,7 +39,7 @@ async def get_cursos():
 
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(default=None, title="O ID do curso", description="Deve ser entre 1 e 3", gt=0, lt=4)):
     try:
         curso = cursos[curso_id]
         return curso
@@ -72,14 +74,26 @@ async def delete_curso(curso_id: int):
     if curso_id in cursos:
         del cursos[curso_id]
         return Response(status_code=status.HTTP_204_NO_CONTENT)
-        #-------------------------------------------------------------------------------------------------
-        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)                                    * 
-        # O JSONResponse tem um bug nessa versão do FastAPI                                              * 
-        #-------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------
+        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)                                    *
+        # O JSONResponse tem um bug nessa versão do FastAPI                                              *
+        # -------------------------------------------------------------------------------------------------
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com o id {curso_id}")
 
+# ---------------------------------------------------------------------------------------------------------
+# Query parameters: 18 Prática Query Parameters_720p                                                      *
+# ---------------------------------------------------------------------------------------------------------
+
+
+@app.get('/calculadora')
+async def calcular(a: int = Query(default=None, gt=5), b: int = Query(default=None, gt=10), c: Optional[int] = None):
+    soma: int = a + b
+    if c:
+        soma = soma + c
+
+    return {"resultado": soma}
 
 if __name__ == "__main__":
     import uvicorn
