7ea51a814762cc04e055a73e24298bd2157a2e5d
renatorv <renato@volus.coms>
2024-09-05 08:40:56 -0300

    finalizada aula 33

---
+++ secao04/api/v1/api.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from api.v1.endpoints import curso
+
+api_router = APIRouter()
+
+api_router.include_router(curso.router, prefix='/cursos', tags=['cursos'])
+# no arquivo configs.py, a variável API_V1_STR é setada para: "/api/v1"
+# assim nosso endereco ficará: /api/v1/cursos [do prefix ]
--- secao04/api/v1/endpoints/curso.py
+++ secao04/api/v1/endpoints/curso.py
@@ -14,17 +14,21 @@ from core.deps import get_session
 
 router = APIRouter()
 
-## O retorno é um CursoScheme, pois o pydantic irá converter para um objeto JSON e devolver para a API,
-## pois a API espera um JSON.
-@router.post('/',status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
+# O retorno é um CursoScheme, pois o pydantic irá converter para um objeto JSON e devolver para a API,
+# pois a API espera um JSON.
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
 async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)):
     # novo_curso = CursoModel(**curso.dict()) testar
-    novo_curso = CursoModel(titulo=curso.titulo, aulas=curso.aulas, horas=curso.horas)
-    
+    novo_curso = CursoModel(titulo=curso.titulo,
+                            aulas=curso.aulas, horas=curso.horas)
+
     db.add(novo_curso)
     await db.commit()
-    
+
     return novo_curso
+
 
 @router.get('/', response_model=List[CursoSchema])
 async def get_cursos(db: AsyncSession = Depends(get_session)):
@@ -32,5 +36,55 @@ async def get_cursos(db: AsyncSession = Depends(get_session)):
         query = select(CursoModel)
         result = await session.execute(query)
         cursos = List[CursoModel] = result.scalars().all()
-    
+
     return cursos
+
+
+@router.get('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db() as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso = result.scalars_one_or_nome()
+
+    if not curso:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+
+    return curso
+
+
+@router.put('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_200_OK)
+async def put_curso(curso_id: int, curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    async with db() as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_up = result.scalars_one_or_nome()
+
+    if curso_up:
+        curso_up.titulo = curso.titulo
+        curso_up.aulas = curso.aulas
+        curso_up.horas = curso.horas
+
+        await session.commit()
+
+        return curso_up
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+        
+@router.delete('/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db() as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del = result.scalars_one_or_nome()
+
+    if curso_del:
+        await session.delete(curso_del)
+        await session.commit()
+
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
