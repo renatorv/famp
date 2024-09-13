cf69c5fa4734088f970801cb6bea01d9c0eabc2b
renatorv <renato@volus.coms>
2024-09-13 08:00:37 -0300

    Criados metodos para aula 42

--- secao05/api/v1/endpoints/curso.py
+++ secao05/api/v1/endpoints/curso.py
@@ -37,6 +37,61 @@ async def get_cursos(db: AsyncSession = Depends(get_session)):
     async with db() as session:
         query = select(CursoModel)
         result = await session.execute(query)
-        
+
         cursos: List[CursoModel] = result.scalars().all()
         return cursos
+
+
+# GET CURSO
+@router.get("/{curso_id}", response_model=CursoModel, status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db() as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso = result.scalars().first()  # ??
+
+    if not curso:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+
+    return curso
+
+
+# PUT CURSO
+@router.put("/{curso_id}", response_model=CursoModel, status_code=status.HTTP_202_ACCEPTED)
+async def put_curso(curso_id: int, curso: CursoModel, db: AsyncSession = Depends(get_session)):
+    async with db() as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_up = result.scalars().first()
+
+        if curso_up:
+            curso_up.titulo = curso.titulo
+            curso_up.aulas = curso.aulas
+            curso_up.horas = curso.horas
+
+            await session.commit()
+
+            return curso_up
+
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+
+
+# DELETE CURSO
+@router.delete("/{curso_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db() as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del = result.scalars().first()
+
+        if curso_del:
+            await session.delete(curso_del)
+            await session.commit()
+
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
